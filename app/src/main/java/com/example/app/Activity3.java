e49a316934003a656b4e42e655432048e4e94aeb
lore <loremiere@gmail.com>
2020-12-29 21:40:13 +0200

    new screen added

--- app/src/main/java/com/example/app/Activity3.java
+++ app/src/main/java/com/example/app/Activity3.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Activity3 extends AppCompatActivity {
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+            submitButton = (Button)findViewById(R.id.button8);
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(Activity3.this, Calculator.class);
+                    startActivity(intent);
+                }
+            });
+        }
 
-    }
 }
